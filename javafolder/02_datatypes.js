c5d1e85620d860b01370dfad951be89388f41ddb
Prabhjot Singh <117665616+Prabj0t@users.noreply.github.com>
2024-03-24 17:54:25 +0000

    datatypes

---
+++ javafolder/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict " // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Hitesh")
+
+// tc39.es  this website is a original documentation for javascript
+
+
+// basic premitive data types
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique    // uniqueness k liye 
+
+
+
+// object
--- javafolder/test.js
+++ javafolder/test.js
@@ -1,2 +1,10 @@
-console.log(5+10+"65"+56+10)
-console.log("happyyyy")
+const accountId = 144123
+let accountEmail = "prabpb31@gmial.com"
+var accountPassword = "12334"
+acountCity = "punjab"
+// use let not var because of scope issue
+// accountId = 3
+accountEmail = "new@gmail.com"
+accountPassword = "1322"
+acountCity = "mansa"
+console.table([accountId , accountEmail , accountPassword , acountCity]);
