d5bd3c7ee7847ab95c6813410d60f1b0d3e94da5
Prabhjot Singh <117665616+Prabj0t@users.noreply.github.com>
2024-03-25 10:02:43 +0000

    comparisons

--- javafolder/Operations.js
+++ javafolder/Operations.js
@@ -10,3 +10,14 @@ console.log("4th ->", 2 ** 2);
 console.log("5th ->", 2 / 2);
 console.log("6th ->", 2 % 2);
 console.log("7th ->", 2);
+
+
+// ========================Comparisons========================
+
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
