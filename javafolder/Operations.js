83c18dece95648f1c3c123aa321e398630247d94
Prabhjot Singh <117665616+Prabj0t@users.noreply.github.com>
2024-03-25 09:58:13 +0000

    learn little operations

---
+++ javafolder/Operations.js
@@ -0,0 +1,12 @@
+let value = 3
+let negativeValue = -value
+
+console.log(negativeValue)
+
+console.log("first ->", 2 + 2);
+console.log("second ->", 2 - 2);
+console.log("third ->", 2 * 2);
+console.log("4th ->", 2 ** 2);
+console.log("5th ->", 2 / 2);
+console.log("6th ->", 2 % 2);
+console.log("7th ->", 2);
