590d8160b07f462c5984ac26a9928187ddd77692
Prabhjot Singh <117665616+Prabj0t@users.noreply.github.com>
2024-04-01 09:16:30 +0000

    new file one.js loops

---
+++ 05_itrations/one.js
@@ -0,0 +1,13 @@
+// for
+
+let array = [1 , 34, 54, 5, 32, 2]
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element);
+}
+
+let myarray = ["happy", "singh","preet"]
+for (let i = 0; i < myarray.length; i++) {
+    const element = myarray[i];
+    console.log(element);
+}
