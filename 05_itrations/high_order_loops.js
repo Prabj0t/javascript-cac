9c7624b0cac7cb63ab355b3d4cccd51030f25be5
Prabhjot Singh <117665616+Prabj0t@users.noreply.github.com>
2024-04-01 10:07:09 +0000

    maps and high order loops

---
+++ 05_itrations/high_order_loops.js
@@ -0,0 +1,26 @@
+
+
+// ["", "", ""] // array 
+// [{}, {}, {}] // objects in array
+
+// for of
+// for (const iterator of object){
+// }
+
+// Iterate through array elements using for...of loop
+const arr = [1, 2, 3, 4, 5];
+for (const num of arr) {
+    console.log(`each number is ${num}`);
+}
+
+// Iterate through characters of a string using for...of loop
+const greetings = "Hello world!";
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+
+map.
