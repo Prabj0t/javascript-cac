ce530f7caf45a164d8cb739c11ccee8656a70494
Prabhjot Singh <117665616+Prabj0t@users.noreply.github.com>
2024-04-02 11:33:19 +0000

    reduce call back function

--- 05_itrations/last_02.js
+++ 05_itrations/last_02.js
@@ -10,3 +10,46 @@ const newNums = myNumers
                 .filter((num) => num >= 40); // Filter numbers greater than or equal to 40
 
 console.log(newNums); // Output: [41, 51, 61, 71, 81, 91]
+
+
+
+// reduce callback function
+
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+// 0 is a start value for accumlators
+const myTotal = myNums.reduce((acc, curr) => acc + curr, 0);
+
+console.log(myTotal); // Output: 6
+
+// Applying reduce to calculate the total price of items in a shopping cart
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+];
+
+// The reduce method is used to accumulate the total price of items in the shopping cart
+// Starting with an initial value of 0, each item's price is added to the accumulator
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0);
+
+console.log(priceToPay); // Output: 22996
